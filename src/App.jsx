a943a425b1af32b07875ba21f4063461245fdef0
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-23 19:08:28 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,52 @@
 import { useState } from 'react'
 
 import './App.css'
-import {Button, Modal} from "@mantine/core";
+import {Button} from "@mantine/core";
 import {useDisclosure} from "@mantine/hooks";
 import EditorModal from "./components/EditorModal.jsx";
-
+import EditorComponent from "./components/EditorComponent.jsx";
+import {createReactEditorJS} from "react-editor-js";
+import {EDITOR_JS_TOOLS} from "./constants.js";
+const ReactEditorJS = createReactEditorJS();
 function App() {
+
   const [count, setCount] = useState(0)
     const [opened, { open, close }] = useDisclosure(false);
-    console.log(opened)
+    //console.log(opened)
   return (
     <>
-        {/*<EditorModal current={opened} status={open}/>*/}
-        <Modal opened={opened} onClose={close} title="EditorModal" centered>
-            <div>
-                <h1>Nacho</h1>
-                <h2>Nacho</h2>
-                <h3>Nacho</h3>
-                <h4>Nacho</h4>
-                <p>nacho</p>
-            </div>
-        </Modal>
+        <EditorModal current={opened} status={close}/>
         <div className={""}>
             <h1>Vite + React {count}</h1>
-            {/*<button onClick={()=> setCount(count+1)}>tryme</button>*/}
+            <button onClick={()=> setCount(count+1)}>tryme</button>
 
             <Button onClick={open}>Open centered Modal</Button>
-            {/*<div className={"relative"}>*/}
-            {/*    <button*/}
-            {/*        className={"bottom-0 rounded-2xl border-2 border-blue-500 p-2 bg-blue-500 text-white"}>*/}
-            {/*        New </button>*/}
-            {/*</div>*/}
         </div>
-
-
+        <EditorComponent />
+        {/*<ReactEditorJS*/}
+        {/*    tools={EDITOR_JS_TOOLS}*/}
+        {/*    defaultValue={{*/}
+        {/*        //time: 1635603431943,*/}
+        {/*        blocks: [*/}
+        {/*            {*/}
+        {/*                id: "sheNwCUP5A",*/}
+        {/*                type: "header",*/}
+        {/*                data: {*/}
+        {/*                    text: "Editor.js",*/}
+        {/*                    level: 2*/}
+        {/*                }*/}
+        {/*            },*/}
+        {/*            {*/}
+        {/*                id: "12iM3lqzcm",*/}
+        {/*                type: "paragraph",*/}
+        {/*                data: {*/}
+        {/*                    text:*/}
+        {/*                        "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text."*/}
+        {/*                }*/}
+        {/*            },*/}
+        {/*        ]*/}
+        {/*    }}*/}
+        {/*/>*/}
     </>
   )
 }
